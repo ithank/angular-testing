9b788b1871bf48806b1e17ccbf32f00d39564aee
ithank <henry.happ@gmail.com>
2018-11-02 10:38:03 -0500

    karma config changes

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+//const isDocker = require('is-docker')();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,7 +27,20 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeHeadless',
+        // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+        // more permissions than Docker allows by default)
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222',],
+      }
+    },
+    singleRun: true
   });
 };
